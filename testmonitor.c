f25b5729127942660f72bcdc8aee7ac86dccdbe6
JonathanMFV <80858858+JonathanMFV@users.noreply.github.com>
2024-03-15 23:19:36 -0600

    Update testmonitor.c

--- testmonitor.c
+++ testmonitor.c
@@ -1,6 +1,8 @@
 #include "synclib.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include "pthread.h"
+#include <unistd.h>
 
 Monitor mon;
 
@@ -8,7 +10,7 @@ void *testMonitor();
 
 int main(){
     int nthreads = 4;
-    pthread_t threads[4];
+    pthread_t threads[nthreads];
 
     monitor_init(&mon);
     
@@ -16,8 +18,29 @@ int main(){
         pthread_create(&threads[i],NULL, testMonitor, NULL);
     }
 
+    for (int i = 0; i < nthreads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    monitor_destroy(&mon);
+    return 0;
 }
 
-void* testMonitor(){
+void *testMonitor(){
     monitor_enter(&mon);
+    printf("Thread %ld entered. \n", pthread_self());   
+    
+    sleep(2);//simulate some work
+        
+    monitor_wait(&mon);
+    printf("Thread %ld woke up.\n", pthread_self());
+
+    monitor_signal(&mon);
+    printf("Thread %ld signaled. \n", pthread_self());
+
+    monitor_exit(&mon);
+    printf("Thread %ld exited. \n", pthread_self());
+
+    return NULL;
+    
 }
